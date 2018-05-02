633d8ee3841431d1c2fa5b8723b2ff97dc7b6126
AlainSanchez <AlainSanchez@10.211.55.3>
2018-05-02 15:55:07 -0600

    cleaning up code

--- src/sudoku/p2p/Main.java
+++ src/sudoku/p2p/Main.java
@@ -26,10 +26,9 @@ import javax.swing.JToolBar;
 import sudoku.dialog.SudokuDialog;
 import sudoku.model.Board;
 import sudoku.model.Solver;
-import sudoku.dialog.SudokuDialog;
 
 @SuppressWarnings("serial")
-public class Main extends SudokuDialog implements NetworkAdapter.MessageListener{
+public class Main extends SudokuDialog implements NetworkAdapter.MessageListener {
 	private ImageIcon NETWORK_OFF, NETWORK_ON;
 	private JButton networkButton;
 	private NetworkAdapter network;
@@ -42,9 +41,9 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 	private JButton sendButton;
 	private JTextArea infoArea;
 	private JToolBar toolBar;
-	
+
 	protected JToolBar createToolBar() {
-		//JToolBar 
+		// JToolBar
 		toolBar = super.createToolBar();
 		NETWORK_OFF = createImageIcon("wifi-orange-right-hi.png");
 		NETWORK_ON = createImageIcon("blue.png");
@@ -55,74 +54,70 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 		toolBar.add(networkButton, toolBar.getComponentCount() - 1);
 		return toolBar;
 	}
-	
-	//checks if the port given is available
+
+	// checks if the port given is available
 	public static boolean portAvailable(int port) {
-    ServerSocket ss = null;
-    DatagramSocket ds = null;
-    try {
-        ss = new ServerSocket(port);
-        ss.setReuseAddress(true);
-        ds = new DatagramSocket(port);
-        ds.setReuseAddress(true);
-        return true;
-    } catch (IOException e) {
-    } finally {
-        if (ds != null) {
-            ds.close();
-        }
+		ServerSocket ss = null;
+		DatagramSocket ds = null;
+		try {
+			ss = new ServerSocket(port);
+			ss.setReuseAddress(true);
+			ds = new DatagramSocket(port);
+			ds.setReuseAddress(true);
+			return true;
+		} catch (IOException e) {
+		} finally {
+			if (ds != null) {
+				ds.close();
+			}
 
-        if (ss != null) {
-            try {
-                ss.close();
-            } catch (IOException e) {
-                /* should not be thrown */
-            }
-        }
-    }
+			if (ss != null) {
+				try {
+					ss.close();
+				} catch (IOException e) {
+					/* should not be thrown */
+				}
+			}
+		}
 
-    return false;
-}
+		return false;
+	}
+
 	private ServerSocket s;
 	private int finalPort = 8000;
-	private void networkButtonClicked(ActionEvent e) { 
-		try{
+
+	private void networkButtonClicked(ActionEvent e) {
+		try {
 			s.close();
 			frame.dispose();
-		}catch(Exception e1){
-			
+		} catch (Exception e1) {
+
 		}
 		int port;
 		port = 8000;
-		while(!portAvailable(port)){
+		while (!portAvailable(port)) {
 			++port;
 		}
 		finalPort = port;
 		createNetGui(finalPort);
 		new Thread(() -> {
 			try {
-				System.out.println("Server started....");
-				
-				//ServerSocket
+
+				// ServerSocket
 				s = new ServerSocket(finalPort);
-				System.out.println("port for server" + finalPort);
-				while(true) {
-					System.out.println("hi1");
+				while (true) {
 					Socket incoming = s.accept();
-					System.out.println("hi2");
 					infoArea.append("Pairing network \n");
-					System.out.println("hi3");
-					System.out.println("Pairing network....");
 					pairAsServer(incoming);
 					infoArea.append(incoming.toString() + "\n");
 				}
-			} catch(Exception ex) {}
+			} catch (Exception ex) {
+			}
 		}).start();
 
 	}
-	
 
-	private void closingButtonClicked(ActionEvent e){
+	private void closingButtonClicked(ActionEvent e) {
 		try {
 			s.close();
 		} catch (IOException e1) {
@@ -130,6 +125,7 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 		}
 		frame.dispose();
 	}
+
 	private JTextField hostIpNameText;
 	private JTextField hostPortNumberText;
 	private JTextField peerNameText;
@@ -138,10 +134,11 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 	InetAddress inet;
 	String hostName;
 	String addr;
+
 	private void createNetGui(int port) {
 
-		//InetAddress 
-		inet = null; 
+		// InetAddress
+		inet = null;
 		hostName = null;
 		addr = null;
 		try {
@@ -152,86 +149,69 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 			e1.printStackTrace();
 		}
 
-
-
-		//JFrame 
+		// JFrame
 		frame = new JFrame("Connection");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(400, 500));
 		frame.setLayout(new BorderLayout());
 		frame.setResizable(false);
 
-
-
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 16, 0, 16));
 		mainPanel.setLayout(new FlowLayout());
 
-
-
 		JPanel hostPanel = new JPanel();
-		hostPanel.setLayout(new FlowLayout(2,8,5));
+		hostPanel.setLayout(new FlowLayout(2, 8, 5));
 		hostPanel.setBorder(BorderFactory.createTitledBorder("Host"));
 		hostPanel.setPreferredSize(new Dimension(350, 125));
 
-
-
 		JPanel peerPanel = new JPanel();
-		peerPanel.setLayout(new FlowLayout(2,8,5));
+		peerPanel.setLayout(new FlowLayout(2, 8, 5));
 		peerPanel.setBorder(BorderFactory.createTitledBorder("Peer"));
 		peerPanel.setPreferredSize(new Dimension(350, 125));
 
-
-
 		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new FlowLayout(0,15,5));
-
-
+		buttonPanel.setLayout(new FlowLayout(0, 15, 5));
 
 		JPanel bottomPanel = new JPanel();
 		bottomPanel.setLayout(new FlowLayout());
 		bottomPanel.setPreferredSize(new Dimension(350, 125));
 
-
 		JLabel hostNameLabel = new JLabel("Host Name:");
-		JTextField hostNameText = new JTextField(hostName,20);
+		JTextField hostNameText = new JTextField(hostName, 20);
 		hostNameText.setEditable(false);
 		JLabel hostIpNameLabel = new JLabel("IP Number:");
-		JTextField hostIpNameText= new JTextField(addr,20);
+		JTextField hostIpNameText = new JTextField(addr, 20);
 		hostIpNameText.setEditable(false);
 		JLabel hostPortNumberLabel = new JLabel("Port Number:");
-		JTextField hostPortNumberText= new JTextField(Integer.toString(port), 20);
+		JTextField hostPortNumberText = new JTextField(Integer.toString(port), 20);
 		hostPortNumberText.setEditable(false);
 
 		JLabel peerNameLabel = new JLabel("Host Name/IP:");
-		peerNameText= new JTextField("10.0.3.7",20);
+		peerNameText = new JTextField("10.0.3.7", 20);
 		JLabel peerPortNumberLabel = new JLabel("Port Number:");
 		peerPortNumberText = new JTextField("8001", 20);
 
+		JButton connectButton = new JButton("Connect"); // start a client - add listeners
+		connectButton.addActionListener(e -> {
+			connectClicked(e);
+		});
 
-
-		JButton connectButton = new JButton("Connect"); //start a client - add listeners
-		connectButton.addActionListener(e-> {connectClicked(e);});
-		
 		JButton disconnectButton = new JButton("Disconnect");
-		
+
 		JButton closeButton = new JButton("Close");
 		closeButton.addActionListener(e -> closingButtonClicked(e));
 
+		infoArea = new JTextArea(6, 30);
+		JScrollPane scroll = new JScrollPane(infoArea);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 
-
-		infoArea = new JTextArea(6,30);
-		JScrollPane scroll = new JScrollPane (infoArea);
-	    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-	          scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
- 
 		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
 		mainPanel.add(hostPanel);
 		mainPanel.add(peerPanel);
 		mainPanel.add(scroll);
 		mainPanel.add(bottomPanel);
-
-
 
 		hostPanel.add(hostNameLabel);
 		hostPanel.add(hostNameText);
@@ -240,8 +220,6 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 		hostPanel.add(hostPortNumberLabel);
 		hostPanel.add(hostPortNumberText);
 
-
-
 		peerPanel.add(peerNameLabel);
 		peerPanel.add(peerNameText);
 		peerPanel.add(peerPortNumberLabel);
@@ -249,56 +227,50 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 		buttonPanel.add(connectButton);
 		buttonPanel.add(disconnectButton);
 		peerPanel.add(buttonPanel);
-		
+
 		disconnectButton.addActionListener(e -> disconnectButtonClicked());
 
 		bottomPanel.add(closeButton);
-
 
 		frame.pack();
 		frame.setVisible(true);
 	}
 
-
-
 	@Override
 	protected void newClicked(int size) {
-    	showMessage("New clicked: " + size);
-    	if( JOptionPane.showConfirmDialog(msgBar, "Would You Like To Play A New Game") == 0){
-    		board = new Board(size); 
-    		boardPanel.setBoard(board);
-            solver = new Solver(board,size);
-            if(network != null) network.writeNew(board.size, board.getSquares());
+		showMessage("New clicked: " + size);
+		if (JOptionPane.showConfirmDialog(msgBar, "Would You Like To Play A New Game") == 0) {
+			board = new Board(size);
+			boardPanel.setBoard(board);
+			solver = new Solver(board, size);
+			if (network != null)
+				network.writeNew(board.size, board.getSquares());
 
-    	}
-    	repaint();
-    
-		
+		}
+		repaint();
+
 	}
 
-
-	 
 	private void disconnectButtonClicked() {
 		try {
 			network.close();
 			infoArea.append("Disconnected \n");
 			return;
-		} catch(Exception ex) {
-			
+		} catch (Exception ex) {
+
 		}
 	}
 
 	@SuppressWarnings("resource")
-	private void connectClicked(ActionEvent e) { 
+	private void connectClicked(ActionEvent e) {
 
 		new Thread(() -> {
 			try {
 				Socket socket = new Socket();
 				infoArea.append("Pairing.... \n");
-				System.out.println("Pairing....");
 				String s = peerNameText.getText();
 				int portn = Integer.parseInt(peerPortNumberText.getText());
-				if(s.equals(inet.getHostAddress()) && portn == finalPort){
+				if (s.equals(inet.getHostAddress()) && portn == finalPort) {
 					JOptionPane.showMessageDialog(null, "Dont Connect to Self");
 					disconnectButtonClicked();
 					infoArea.append("Disconnected \n");
@@ -306,187 +278,170 @@ public class Main extends SudokuDialog implements NetworkAdapter.MessageListener
 				}
 				socket.connect(new InetSocketAddress(s, portn), 5000);
 				pairAsClient(socket);
-			} catch(Exception ex) {}
-
-
+			} catch (Exception ex) {
+			}
 
 		}).start();
 
-
-
 	}
-	
+
 	@Override
-	protected void solve(){
-		if(solver.solve()){
-			System.out.println("SOLVE");
+	protected void solve() {
+		if (solver.solve()) {
 			showMessage("Solved");
-			if (network != null){
-				System.out.println("studf");
+			if (network != null) {
 				int[] b = board.getSquares();
-				System.out.println(b.length);
-				System.out.println("bsquare");
-				for(int i = 0; i < b.length; i+=4){
-					System.out.println("studf3456");
-					network.writeFill(b[i], b[i+1], b[i+2]); 
-					System.out.println(i+3);
-			
+				for (int i = 0; i < b.length; i += 4) {
+					network.writeFill(b[i], b[i + 1], b[i + 2]);
+
 				}
 			}
 			repaint();
 			congratulate();
-		}
-		else {showMessage("Not Solvable");
+		} else {
+			showMessage("Not Solvable");
 			errSound();
 		}
 		repaint();
 	}
-	
+
 	@Override
 	public void congratulate() {
 		JOptionPane.showMessageDialog(null, "Congratulations!");
 	}
-	
+
 	@Override
 	public void setValues(boolean boo) {
-			board.setCoordinates(values, boo);
-			if (network != null) { network.writeFill(values[0], values[1], values[2]); } 
+		board.setCoordinates(values, boo);
+		if (network != null) {
+			network.writeFill(values[0], values[1], values[2]);
+		}
 	}
 
-	private void pairAsClient(Socket socket) { //sync
-		   network = new NetworkAdapter(socket);
-		   network.setMessageListener(this);
-		   network.writeJoin();
-		   network.receiveMessages(); // loop till disconnected
-	}		
-	
-	private void pairAsServer(Socket socket) { //sync
-		   network = new NetworkAdapter(socket);
-		   network.setMessageListener(this);
-		   network.receiveMessages(); // loop till disconnected
-		}
-	
-	
-	
-	public static void main(String[] args){
+	private void pairAsClient(Socket socket) { // sync
+		network = new NetworkAdapter(socket);
+		network.setMessageListener(this);
+		network.writeJoin();
+		network.receiveMessages(); // loop till disconnected
+	}
+
+	private void pairAsServer(Socket socket) { // sync
+		network = new NetworkAdapter(socket);
+		network.setMessageListener(this);
+		network.receiveMessages(); // loop till disconnected
+	}
+
+	public static void main(String[] args) {
 		new Main();
 	}
-	
-	public void addMessage(String msg){
-		
+
+	public void addMessage(String msg) {
+
 	}
-	
+
 	@Override
 	public void messageReceived(sudoku.p2p.NetworkAdapter.MessageType type, int x, int y, int z, int[] others) {
-		System.out.println(type);
-		switch(type) {
+		switch (type) {
 		case FILL:
-			System.out.println("FILL:");
 			int xtemp = values[0];
 			int ytemp = values[1];
 			values[0] = x;
 			values[1] = y;
 			values[2] = z;
-			infoArea.append("<= "+type +" "+ x  +" "+ y +" "+  z + "\n");
-			boardPanel.highlightPeer(x,y);
+			infoArea.append("<= " + type + " " + x + " " + y + " " + z + "\n");
+			boardPanel.highlightPeer(x, y);
 			board.setCoordinates(values, true);
 
 			values[0] = xtemp;
 			values[1] = ytemp;
 			repaint();
-	        if(board.isSolved())
-	        	congratulate();
-			
+			if (board.isSolved())
+				congratulate();
 			break;
-
-
 
 		case JOIN:
-			//create board to share"
+			// create board to share"
 			waitingForJoin();
-			infoArea.append("<= "+type + "\n");
-			System.out.println("JOIN");
+			infoArea.append("<= " + type + "\n");
 			int[] square = board.getSquares();
-	    	if( JOptionPane.showConfirmDialog(msgBar, "Would you like to share your game", "Share", JOptionPane.YES_NO_OPTION) == 0){
-	    		wait.dispose();
-	    		network.writeJoinAck(board.size, square);
+			if (JOptionPane.showConfirmDialog(msgBar, "Would you like to share your game", "Share",
+					JOptionPane.YES_NO_OPTION) == 0) {
+				wait.dispose();
+				network.writeJoinAck(board.size, square);
 				networkButton.setIcon(NETWORK_ON);
-				infoArea.append("=> "+"JOIN_ACK: " + 1 + ","+board.size+",");
-				for(int i: board.getSquares())
-					infoArea.append(i+",");
-	    	}
-	    	else{
-	    		wait.dispose();
-	    		network.writeJoinAck();
-	    		infoArea.append("=> "+"JOIN_ACK: " + 0 + " \n");
-	    	}
+				infoArea.append("=> " + "JOIN_ACK: " + 1 + "," + board.size + ",");
+				for (int i : board.getSquares())
+					infoArea.append(i + ",");
+			} else {
+				wait.dispose();
+				network.writeJoinAck();
+				infoArea.append("=> " + "JOIN_ACK: " + 0 + " \n");
+			}
 			break;
+
 		case JOIN_ACK:
-			System.out.println("JOIN_ACK");
-			if(x == 0){ 
-				System.out.println("Reject JOIN");
+			if (x == 0) {
 				disconnectButtonClicked();
-				break;}
+				break;
+			}
 			networkButton.setIcon(NETWORK_ON);
-			infoArea.append("<= "+type + x +" "+ y +" ");
-				for(int i = 0; i < others.length; i++)
-					infoArea.append(others[i] + " ");
-				infoArea.append(" \n");
-			newClicked(y, others);
-			break;
-		case NEW:
-			System.out.println("NEW");
-			infoArea.append("<= "+type + x +" "+ y +" ");
-			for(int i = 0; i < others.length; i++)
+			infoArea.append("<= " + type + x + " " + y + " ");
+			for (int i = 0; i < others.length; i++)
 				infoArea.append(others[i] + " ");
 			infoArea.append(" \n");
-				if( JOptionPane.showConfirmDialog(msgBar, "Would you like to start a New Game with Player", "New Game", JOptionPane.YES_NO_OPTION) == 0){
-					newClicked(x, others);
-		    		infoArea.append("=> "+"new_ACK: " + 1 + " \n");
-				}
-		    	else{
-		    		try {
-		    			network.close();
-		    			infoArea.append("Disconnected \n");
-		    			return;
-		    		} catch(Exception ex) {
-		    			
-		    		}
-		    		infoArea.append("=> "+"new_ACK: " + 0 + " \n");
-		    	}
-				break;
-				
-		case NEW_ACK: 
-			System.out.println("NEW_ACK");
-			infoArea.append("<= "+type + x +" "+ y +" ");
-				for(int i = 0; i < others.length; i++)
-					infoArea.append(others[i] + " ");
-				infoArea.append(" \n");
 			newClicked(y, others);
 			break;
 			
+		case NEW:
+			infoArea.append("<= " + type + x + " " + y + " ");
+			for (int i = 0; i < others.length; i++)
+				infoArea.append(others[i] + " ");
+			infoArea.append(" \n");
+			if (JOptionPane.showConfirmDialog(msgBar, "Would you like to start a New Game with Player", "New Game",
+					JOptionPane.YES_NO_OPTION) == 0) {
+				newClicked(x, others);
+				infoArea.append("=> " + "new_ACK: " + 1 + " \n");
+			} else {
+				try {
+					network.close();
+					infoArea.append("Disconnected \n");
+					return;
+				} catch (Exception ex) {
+
+				}
+				infoArea.append("=> " + "new_ACK: " + 0 + " \n");
+			}
+			break;
+
+		case NEW_ACK:
+			infoArea.append("<= " + type + x + " " + y + " ");
+			for (int i = 0; i < others.length; i++)
+				infoArea.append(others[i] + " ");
+			infoArea.append(" \n");
+			newClicked(y, others);
+			break;
+
 		case CLOSE:
 			networkButton.setIcon(NETWORK_OFF);
-			
 			break;
-			
+
 		default:
 			break;
 		}
 
-		
 	}
-	
+
 	JFrame wait;
+
 	private void waitingForJoin() {
-		 wait = new JFrame("Share");
+		wait = new JFrame("Share");
 
 		ImageIcon waiting = createImageIcon("91.gif");
-	    wait.add(new JLabel("Waiting for other player"), BorderLayout.NORTH);
-	    wait.add(new JLabel(waiting));
+		wait.add(new JLabel("Waiting for other player"), BorderLayout.NORTH);
+		wait.add(new JLabel(waiting));
 
-	    wait.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	    wait.setSize(350, 300);
-	    wait.setVisible(true);
+		wait.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		wait.setSize(350, 300);
+		wait.setVisible(true);
 	}
 }
